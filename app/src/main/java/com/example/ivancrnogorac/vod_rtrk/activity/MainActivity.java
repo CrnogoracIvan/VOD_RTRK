b7086109297cc17defa5da6ae2083ac2f5d9e8a0
CrnogoracIvan <crrnii@yahoo.com>
2017-07-09 17:33:22 +0200

    App basics created

---
+++ app/src/main/java/com/example/ivancrnogorac/vod_rtrk/activity/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.ivancrnogorac.vod_rtrk.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.example.ivancrnogorac.vod_rtrk.R;
+import com.example.ivancrnogorac.vod_rtrk.adapter.ImageAdapter;
+import com.example.ivancrnogorac.vod_rtrk.model.MovieList;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        /**-----5 seconds Splash Sceen---*/
+        ImageView logoImage = new ImageView(this);
+        logoImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        logoImage.setImageResource(R.drawable.rt_rk_logo);
+        setContentView(logoImage);
+        Handler h = new Handler();
+
+        int SPLASH_DISPLAY_LENGTH = 5000;
+        h.postDelayed(new Runnable() {
+            public void run() {
+                setContentView(R.layout.activity_main);
+                imgLoader();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    /**------------------------------------------------------*/
+
+    /** String keys for Intent */
+
+    public static final String IMAGE = "image";
+    public static final String DESCRIPTION = "desc";
+    public static final String BACKGROUND = "background";
+    public static final String ACTOR = "actor";
+    public static final String MOVIE_NAME = "movie_name";
+    public static final String VIDEO_TRAILER = "video_trailer";
+    public static final String URL = "movie_url";
+
+
+    private Integer moviesImages[] = {
+
+            R.drawable.the_wolf_of_wall_street,
+            R.drawable.rush,
+            R.drawable.dark_knight_rises,
+            R.drawable.how_to_train_your_dragon_2,
+            R.drawable.the_expendables_3,
+            R.drawable.the_amazing_spider_man_2,
+            R.drawable.the_22_jump_street,
+            R.drawable.guardians_of_the_galaxy,
+            R.drawable.maleficent,
+            R.drawable.toy_story_3,
+            R.drawable.despicable_me_2,
+            R.drawable.space_jam,
+            R.drawable.godzilla,
+            R.drawable.what_if,
+            R.drawable.lets_be_cops
+    };
+
+    /** imgLoader created to ensure showing proper images with GridView component.
+     This method provided click on chosen movie from MainActivity film list */
+
+    private void imgLoader() {
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        ImageAdapter imageAdapter = new ImageAdapter(MainActivity.this, moviesImages);
+        gridView.setAdapter(imageAdapter);
+
+        /** This onItemClick ( Intent in OnItemClick ) will send positioned details about clicked movie.
+         This details will show on MovieDetailsActivity */
+
+//        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Intent intent = new Intent(MainActivity.this, MovieDetailsActivity.class);
+//                intent.putExtra(IMAGE, MovieList.setupMovies().get(position).getImage());
+//                intent.putExtra(DESCRIPTION, MovieList.setupMovies().get(position).getDescription());
+//                intent.putExtra(BACKGROUND, MovieList.setupMovies().get(position).getBgImage());
+//                intent.putExtra(ACTOR, MovieList.setupMovies().get(position).getActors());
+//                intent.putExtra(MOVIE_NAME, MovieList.setupMovies().get(position).getMovie());
+//                intent.putExtra(VIDEO_TRAILER, MovieList.setupMovies().get(position).getVideoUrl());
+//                intent.putExtra(URL, MovieList.setupMovies().get(position).getUrl());
+//
+//                startActivity(intent);
+//            }
+//        });
+
+    }
+}
---
+++ app/src/main/java/com/example/ivancrnogorac/vod_rtrk/model/Movie.java
@@ -0,0 +1,90 @@
+package com.example.ivancrnogorac.vod_rtrk.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Ivan Crnogorac on 7/9/2017.
+ */
+
+public class Movie {
+
+    private String movie;
+    private String image;
+    private String bgImage;
+    private String videoUrl;
+    private String description;
+    private String actors;
+    private String url;
+
+    public Movie() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getBgImage() {
+        return bgImage;
+    }
+
+    public void setBgImage(String bgImage) {
+        this.bgImage = bgImage;
+    }
+
+    public String getMovie() {
+        return movie;
+    }
+
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "movie='" + movie + '\'' +
+                ", image='" + image + '\'' +
+                ", bgImage='" + bgImage + '\'' +
+                ", videoUrl='" + videoUrl + '\'' +
+                ", description='" + description + '\'' +
+                ", actors='" + actors + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
